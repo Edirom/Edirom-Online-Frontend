51e64eda30f161a0b89ee3eddc35ba885de104d3
Daniel Jettka <research@jettka.de>
2026-03-05 14:59:51 +0100

    reverted previous change because it fails in Chrome

--- app/view/desktop/TaskBar.js
+++ app/view/desktop/TaskBar.js
@@ -240,6 +240,7 @@ Ext.define('EdiromOnline.view.desktop.TaskBar', {
 
     getWindowBarConfig: function () {
         return {
+            flex: 1,
             cls: 'ux-desktop-windowbar',
             items: [ '&#160;' ],
             layout: { overflowHandler: 'Scroller' }
