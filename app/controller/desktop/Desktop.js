e2f086e51859f6056a74671544c3cb91116868c8
Dennis Friedl <DennisFriedl@paderborn.com>
2026-08-12 12:08:32 +0200

    reduced code for Concordance Navigator start

--- app/controller/desktop/Desktop.js
+++ app/controller/desktop/Desktop.js
@@ -51,17 +51,10 @@ Ext.define('EdiromOnline.controller.desktop.Desktop', {
         
         var concNavOnStart = window.getPreference('concordance_navigator_open_on_start', true);
         var hasConnectionParam = this.application.activeConnection != null;
-        if(concNavOnStart != null && concNavOnStart) {
-            this.desktop.taskbar.setConcordanceNavigatorButtonToggleState(true, true);
-            Ext.defer(this.openConcordanceNavigator, 1000, this);
-        } else if(hasConnectionParam) {
+        if ((concNavOnStart != null && concNavOnStart) || hasConnectionParam) {
             Ext.defer(this.openConcordanceNavigator, 1000, this);
         }
-        // if(concNavOnStart != null && concNavOnStart) {
-        //     this.desktop.taskbar.setConcordanceNavigatorButtonToggleState(true, true);
-        //     Ext.defer(this.openConcordanceNavigator, 1000, this);
-        // }
-        
+
         this.desktop.taskbar.addListener('openHelp', this.openHelp, this);
         //TODO: Suchfenster einbauen
         /*this.desktop.taskbar.addListener('openSearchWindow', this.openSearchWindow, this);*/
